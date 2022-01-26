eb0dbcd14883efb64df7a224e91b88e3ef94ee5d
mitchr <m.riley@temple.edu>
2022-01-25 20:01:48 -0500

    Defer loading of plotly and katex

--- js/katexConfig.js
+++ js/katexConfig.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-		renderMathInElement(document.body, {
-			delimiters: [
-				{left: "$$", right: "$$", display: true},
-				{left: "$", right: "$", display: false},
-		]});
-});
+function katexLoad() {
+	renderMathInElement(document.body, {
+		delimiters: [
+			{ left: "$$", right: "$$", display: true },
+			{ left: "$", right: "$", display: false },
+		]
+	});
+}
